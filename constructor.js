4ebabb2aa1ab42c16c5ece55ec55763b7a4d15a3
S1bu <Sibusisodyan@gmail.com>
2023-07-31 16:12:27 +0200

    done

---
+++ constructor.js
@@ -0,0 +1,10 @@
+ 
+//My constructor function  
+ function person(firstName, lastName, emailAddress) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailAddress = emailAddress;
+  }
+ 
+//   exporting
+module.exports = person;
---
+++ index.js
@@ -0,0 +1,12 @@
+//  import
+const person = require('./constructor');
+ 
+const person1 = new person('Sibusiso', 'Dyan', 'Sibusisodyan@gmail.com');
+const person2 = new person('Taflynne', 'Petersen', 'Taflynnepetersen@gmail.com');
+const person3 = new person('Kgodisho','Lebopa','KgodishoLebopa@gmail.com')
+ 
+console.log(person1);
+console.log(person2);
+console.log(person3);
+ 
+ 
